78294c79d7d80435041500d37abc871e3d1cccdd
matec <m.gerard@student.maastrichtuniversity.nl>
2024-09-09 16:31:27 +0200

    create a DB

---
+++ test.sql
@@ -0,0 +1,12 @@
+-- Create the pizza ordering database
+CREATE DATABASE PizzaOrderingSystem;
+USE PizzaOrderingSystem;
+
+-- Table for storing different pizzas
+CREATE TABLE Pizzas (
+    pizza_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    description TEXT,
+    is_vegetarian BOOLEAN DEFAULT FALSE,
+    is_vegan BOOLEAN DEFAULT FALSE
+);
