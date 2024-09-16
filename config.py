cadc118aa168791c14f5f83c5bbd0b09d6437ee8
matec <m.gerard@student.maastrichtuniversity.nl>
2024-09-16 12:14:27 +0200

    literally the whole project done LMAO

--- config.py
+++ config.py
@@ -1,2 +1,11 @@
-password = 'passward'
-username = 'root'
+# config.py
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Load environment variables from a .env file
+
+DATABASE_USERNAME = os.getenv('DATABASE_USERNAME', 'root')
+DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD', 'IlikeKatyPerry')
+DATABASE_HOST = os.getenv('DATABASE_HOST', 'localhost')
+DATABASE_PORT = os.getenv('DATABASE_PORT', '3306')
+DATABASE_NAME = os.getenv('DATABASE_NAME', 'PizzaOrderingSystem')
